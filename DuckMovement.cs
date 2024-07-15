8ecb17ac4a5044d212f7a1c01ff65cf2d81c3e53
rapidswap <73596570+rapidswap@users.noreply.github.com>
2024-07-16 05:04:35 +0900

    움직임 변경

--- DuckMovement.cs
+++ DuckMovement.cs
@@ -4,59 +4,63 @@ using UnityEngine;
 
 public class DuckMovement : MonoBehaviour
 {
-    public float moveSpeed = 3f; // 이동 속도 조절용 변수
-    //public float rotationSpeed = 720f; // 회전 속도 조절용 변수
-    Animator anim;
+    Vector3 dir;
 
-    void Awake()
+    Animator anim;
+    CharacterController cc;
+
+    public AudioClip footstep;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        anim = GetComponentInChildren<Animator>();
+        anim = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        Vector2 thumbstick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
-        Debug.Log("Thumbstick Input: " + thumbstick);
-
-        // 이동 방향 설정
-        Vector3 moveDirection = new Vector3(thumbstick.x, 0f, thumbstick.y);
-
-        // 이동 벡터의 길이가 0.1보다 크면 이동 (대각선 이동 포함)
-        if (moveDirection.sqrMagnitude > 0.01f)
+        // 캐릭터가 지면에 있는 경우
+        if (cc.isGrounded)
         {
-            // 각 방향에 대한 이동 처리
-            Vector3 direction = Vector3.zero;
+            float h = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x; // 좌우 이동
+            float v = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).y; // 앞뒤 이동
 
-            if (thumbstick.x > 0.1f) // 오른쪽 입력 (직진)
-            {
-                direction += Vector3.forward;
-            }
-            else if (thumbstick.x < -0.1f) // 왼쪽 입력 (후진)
-            {
-                direction += Vector3.back;
-            }
+            dir = new Vector3(h, 0, v) * speed;
 
-            if (thumbstick.y > 0.1f) // 위쪽 입력 (왼쪽으로 이동)
+            if (dir.magnitude > 0.1f)
             {
-                direction += Vector3.left;
+                // 진행 방향으로 캐릭터 회전
+                float targetAngle = Mathf.Atan2(h, v) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+                anim.SetBool("isWalk", true);
             }
-            else if (thumbstick.y < -0.1f) // 아래쪽 입력 (오른쪽으로 이동)
+            else
             {
-                direction += Vector3.right;
+                anim.SetBool("isWalk", false);
             }
 
-            // 오브젝트 이동
-            transform.Translate(direction.normalized * moveSpeed * Time.deltaTime, Space.World);
-
-            // 오브젝트 회전
-            //Quaternion targetRotation = Quaternion.LookRotation(direction.normalized);
-            //transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-            anim.SetBool("isWalk", true);
+            // 점프 처리
+            if (OVRInput.GetDown(OVRInput.Button.Three)) // Oculus Quest의 경우 OVRInput.Button.Two은 버튼 'X'에 해당
+            {
+                dir.y = 7.5f; // 점프 높이
+                anim.SetBool("isJump",true); // 점프 애니메이션 
+                
+            }
+            else
+            {
+                anim.SetBool("isJump", false);
+            }
         }
-        else
-        {
-            anim.SetBool("isWalk", false);
-        }
+
+        dir.y += Physics.gravity.y * Time.deltaTime;
+        cc.Move(dir * Time.deltaTime);
+    }
+
+    void FootStep()
+    {
+        AudioSource.PlayClipAtPoint(footstep, Camera.main.transform.position);
     }
 }
